ab2f6510b6a7f89e5bb88d28135f66059a3ee783
Yusadolat <yusadolat@gmail.com>
2021-04-05 16:39:41 +0100

    refactoring with async Handelr..

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const authController = require('./auth.controller');
 const userController = require('./user.controller');
+const fastController = require('./fast.controller')
 
 module.exports = {
     authController,
     userController,
+    fastController
 }
--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 const User = require('./user');
+const Fast = require('./fast');
 
 module.exports = {
     User,
+    Fast
 }
