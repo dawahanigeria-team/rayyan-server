adaa96190903b83b14ff2d830929b5eede797681
Yusadolat <yusadolat@gmail.com>
2021-04-05 16:25:06 +0100

    Added Gravetar :zap:

--- models/user.js
+++ models/user.js
@@ -1,36 +1,41 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
+const UserSchema = new Schema(
+  {
     firstName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     lastName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: [true, 'Please enter an email'],
-        unique: true,
-        lowercase: true,
+      type: String,
+      required: [true, "Please enter an email"],
+      unique: true,
+      lowercase: true,
     },
     password: {
-        type: String,
+      type: String,
     },
     picture: {
-        type: String,
+      type: String,
+      default:
+        "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50.jpg",
     },
     isAdmin: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
     isConfirmed: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
 UserSchema.statics.isEmailTaken = async function (email, excludeUserId) {
     const user = await this.findOne({ email, _id: { $ne: excludeUserId } });
