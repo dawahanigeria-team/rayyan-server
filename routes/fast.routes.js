cc35ad7075d079833fce8143114d29bf02666747
Yusadolat <yusadolat@gmail.com>
2021-04-26 22:13:41 +0100

    Added Update Routes :zap:

--- routes/fast.routes.js
+++ routes/fast.routes.js
@@ -4,6 +4,7 @@ const {
   GetSingleFast,
   getFasts,
   getMissedFasts,
+  updateSingleFast,
 } = require("./../controllers/fast.controller");
 const { requireAuth } = require("../middlewares/auth.middleware");
 
@@ -12,7 +13,7 @@ const router = Router();
 
 router.route("/").post(createNewFast).get(getFasts);
 router.route("/missedfast").get(getMissedFasts)
-router.route("/:id").get(GetSingleFast);
+router.route("/:id").get(GetSingleFast).put(updateSingleFast);
 
 
 module.exports = router;
