ec8003e52451485c55db685e7cfccceceb542827
Yusadolat <yusadolat@gmail.com>
2021-05-09 14:08:56 +0100

    bulk fast added :zap:

--- controllers/fast.controller.js
+++ controllers/fast.controller.js
@@ -10,6 +10,27 @@ module.exports.createNewFast = asyncHandler(async (req, res) => {
   res.status(201).json({ success: true, data: { fast } });
 });
 
+// @desc Post bull Fast
+// @route Post /api/fast/bulkfasts?number
+// @access Private
+
+module.exports.createBulkFast = asyncHandler(async (req, res) => {
+  const { description, user, number } = req.body;
+
+  var bulkfasts = [];
+  for (var i = 1; i <= number; ++i) {
+    var newFast = {
+      name: `Missed Fast ${Math.random()}`,
+      description: description,
+      user: user,
+    };
+
+    bulkfasts.push(newFast);
+  }
+  const fasts = await Fast.collection.insertMany(bulkfasts);
+  res.status(201).json({ success: true, data: { fasts } });
+});
+
 // @desc Update user profile
 // @route PUT /api/fast/:id
 // @access Private
@@ -21,47 +42,44 @@ module.exports.GetSingleFast = asyncHandler(async (req, res) => {
 // @desc    Get Missed Fast
 // @route   GET /api/fast/missedfast
 // @access  Private/Admin
-module.exports.getMissedFasts = asyncHandler( async (req, res) => {
-  const user = req.query.user
+module.exports.getMissedFasts = asyncHandler(async (req, res) => {
+  const user = req.query.user;
   const fast = await Fast.find({ user: user, status: false });
 
-  res.status(200).json({success: true, data: { fast}});
-  
-})
+  res.status(200).json({ success: true, data: { fast } });
+});
 
 // @desc    Get all fasts
 // @route   GET /api/fasts
 // @access  Private/Admin
-module.exports.getFasts = asyncHandler( async (req, res) => {
-    const user  =  req.query.user
-    const fasts = await Fast.find({ user: user});
-    res.status(200).send(fasts);
-  
+module.exports.getFasts = asyncHandler(async (req, res) => {
+  const user = req.query.user;
+  const fasts = await Fast.find({ user: user });
+  res.status(200).json({ status: true, total: fasts.length, data: { fasts } });
 });
 
 // @desc    Get all fasts
 // @route   GET /api/fasts
 // @access  Private/Admin
 
-module.exports.getFasts = asyncHandler( async (req, res) => {
-const user = req.query.user;
-    const fasts = await Fast.find({user: user});
-    res.status(200).send(fasts);
- 
-});
+// module.exports.getFasts = asyncHandler( async (req, res) => {
+// const user = req.query.user;
+//     const fasts = await Fast.find({user: user});
+//     res.status(200).send(fasts);
+
+// });
 
 // @desc    Update fast by ID
 // @route   GET /api/fast/:id
 // @access  Private/Admin
-module.exports.updateSingleFast = asyncHandler( async (req, res) => {
- const id = req.params.id;
-    const fast = await Fast.findOneAndUpdate(
-      { _id: id },
-      { $set: { status: true } },
-      { new: true }
-    );
-    res.status(200).json({ success: true, data: { fast }});
-  
+module.exports.updateSingleFast = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  const fast = await Fast.findOneAndUpdate(
+    { _id: id },
+    { $set: { status: true } },
+    { new: true }
+  );
+  res.status(200).json({ success: true, data: { fast } });
 });
 
 // @desc    Delete fast
